d96e3072f662599ec0b1e0b84189de64d025c674
Nikolai <niklonberg@gmail.com>
2024-06-17 13:37:21 +0200

    add header & nav elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { appleImg, bagImg, searchImg } from "../utils";
 
 const Navbar = () => {
-  return <div>Navbar here</div>;
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="apple logo" width={24} />
+
+        <ul>
+          {["Phones", "Tablets", "Macbooks"].map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+
+        <div>
+          <img src={searchImg} alt="search" width={24} />
+          <img src={bagImg} alt="bag" width={24} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
