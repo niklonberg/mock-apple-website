7982248c45c9807aa01a5cc729ebee2daec618dc
Nikolai <niklonberg@gmail.com>
2024-06-18 09:31:03 +0200

    install gsap, add hero title animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to('.hero-title', {
+      delay: 1.5,
+      duration: 1,
+      opacity: 1,
+    });
+  }, []);
+
   return (
     <section className="nav-height relative bg-black">
-      <div className="h-5/6">
+      <div className="flex-center h-5/6 flex-col">
         <p className="hero-title">iPhone 15 Pro</p>
       </div>
     </section>
