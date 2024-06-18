449da4ecbd1d98795bb1cd8a79af2dafb32437f5
Nikolai <niklonberg@gmail.com>
2024-06-18 10:29:24 +0200

    feat: edit Highlights component & Add GSAP animations

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { rightImg, watchImg } from '../utils';
 
 const Highlights = () => {
-  return <div id="highlights">Highlights here</div>;
+  useGSAP(() => {
+    gsap.to('#title', {
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to('.link', {
+      opacity: 1,
+      y: 0,
+    });
+  });
+  return (
+    <section id="highlights" className="common-padding h-full overflow-hidden bg-zinc">
+      <div className="screen-max-width">
+        <div className="mb-12 items-end justify-between md:flex">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-4">
+            <a className="link">
+              Watch the film <img className="ml-2" src={watchImg} alt="" />
+            </a>
+            <a className="link">
+              Watch the event <img className="ml-2" src={rightImg} alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Highlights;
