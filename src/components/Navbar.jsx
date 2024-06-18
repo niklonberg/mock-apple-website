eca0fcabd3d0233aaeab3565a28bddabd2b7d57d
Nikolai <niklonberg@gmail.com>
2024-06-18 09:11:02 +0200

    style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { appleImg, bagImg, searchImg } from '../utils';
+import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
@@ -6,13 +7,15 @@ const Navbar = () => {
       <nav className="screen-max-width flex w-full">
         <img src={appleImg} alt="apple logo" width={24} />
 
-        <ul>
-          {['Phones', 'Tablets', 'Macbooks'].map((item) => (
-            <li key={item}>{item}</li>
+        <ul className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((item) => (
+            <li className="cursor-pointer px-4 text-sm text-gray transition-all hover:text-white" key={item}>
+              {item}
+            </li>
           ))}
         </ul>
 
-        <div>
+        <div className="flex items-baseline gap-6 max-sm:flex-1 max-sm:justify-end">
           <img src={searchImg} alt="search" width={24} />
           <img src={bagImg} alt="bag" width={24} />
         </div>
