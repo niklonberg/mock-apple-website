10d1c092897cd8f2ac6ff78d453cbd562dd32cc1
Nikolai <niklonberg@gmail.com>
2024-06-18 09:23:29 +0200

    chore: Update Hero component with new design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    <div>
-      Hero here
-    </div>
-  )
-}
+    <section className="nav-height relative bg-black">
+      <div className="h-5/6">
+        <p className="hero-title">iPhone 15 Pro</p>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
