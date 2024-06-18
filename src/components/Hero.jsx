c0a74e27aba41ee1069af9200865cd101e7fc6cb
Nikolai <niklonberg@gmail.com>
2024-06-18 09:46:31 +0200

    add video element and videoSrc state

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = React.useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+
   useGSAP(() => {
     gsap.to('.hero-title', {
       delay: 1.5,
@@ -15,6 +18,11 @@ const Hero = () => {
     <section className="nav-height relative bg-black">
       <div className="flex-center h-5/6 flex-col">
         <p className="hero-title">iPhone 15 Pro</p>
+        <div className="w-9/12 md:w-10/12">
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   );
