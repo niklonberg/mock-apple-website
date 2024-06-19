d4539c150315abb3a92e0564d59bf9eac2c89971
Nikolai <niklonberg@gmail.com>
2024-06-19 16:10:48 +0200

    install and config sentry

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
+import * as Sentry from '@sentry/react';
 
 const App = () => {
+  //eslint-disable-next-line
+  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
   return (
     <main>
       <Navbar />
@@ -15,4 +19,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-})
+  plugins: [
+    react(),
+    eslint(),
+    sentryVitePlugin({
+      org: 'jsm-z3',
+      project: 'javascript-react',
+    }),
+  ],
+
+  build: {
+    sourcemap: true,
+  },
+});
