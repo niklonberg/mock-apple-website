dfb6cc4d34d0caa5780cc3d5aa18382106ae9699
Nikolai <niklonberg@gmail.com>
2024-06-19 14:50:26 +0200

    fix view layout

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -7,7 +7,7 @@ import ModelLoader from './ModelLoader';
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotation, item, size }) => {
   return (
-    <View index={index} id={gsapType} className={`h-full w-full ${index === 1 ? 'right-[-100%]' : ''}`}>
+    <View index={index} id={gsapType} className={`absolute h-full w-full ${index === 1 ? 'right-[-100%]' : ''}`}>
       {/* Ambient light */}
       <ambientLight intensity={0.5} />
 
