2e6d07617361c50cb983a07f9cb03108ae2728d5
Nikolai <niklonberg@gmail.com>
2024-06-19 10:57:00 +0200

    add Model, ModelView components

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,30 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import React from 'react';
+import ModelView from './ModelView';
+import { yellowImg } from '../utils';
+import * as THREE from 'three';
 
 const Model = () => {
+  const [size, setSize] = React.useState('small');
+  const [model, setModel] = React.useState({
+    title: 'iPhone 15 pro in Titanium',
+    color: ['8F8A81', '#FFE7B9', '#6F6C64'],
+    img: yellowImg,
+  });
+
+  // camera control for model view
+  const cameraControlSmall = React.useRef();
+  const cameraControlLarge = React.useRef();
+
+  // models
+  const small = React.useRef(new THREE.Group());
+  const large = React.useRef(new THREE.Group());
+
+  // rotation
+  const [smallRotation, setSmallRotation] = React.useState(0);
+  const [largeRotation, setLargeRotation] = React.useState(0);
+
   useGSAP(() => {
     gsap.to('#heading', {
       duration: 1,
@@ -18,7 +40,9 @@ const Model = () => {
         </h2>
 
         <div className="mt-5 flex flex-col items-center">
-          <div className="relative h-[75vh] overflow-hidden md:h-[90]"></div>
+          <div className="relative h-[75vh] overflow-hidden md:h-[90]">
+            <ModelView />
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/ModelView.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ModelView = () => {
+  return <div>hello</div>;
+};
+
+export default ModelView;
