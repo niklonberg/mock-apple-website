72f1407593e2a8edb8138f57b62afa9724017db2
Nikolai <niklonberg@gmail.com>
2024-06-19 12:22:56 +0200

    add ModelView component

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { PerspectiveCamera, View } from '@react-three/drei';
+import Lights from './Lights';
+import Model from './iPhoneModel';
 
-const ModelView = () => {
-  return <div>hello</div>;
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotation, item, size }) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`h-full w-full border-2 border-red-500 ${index === 1 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient light */}
+      <ambientLight intensity={0.5} />
+
+      {/* Camera */}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <Suspense fallback={<div>Loading</div>}>
+        <Model />
+      </Suspense>
+    </View>
+  );
 };
 
 export default ModelView;
